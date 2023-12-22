6ef43e33b313ccc8c795084c9b2c844b4671df81
Jatin Satani <jatinsatani111@gmail.com>
2023-12-22 15:01:07 +0530

    Done

---
+++ 5program.py
@@ -0,0 +1,13 @@
+
+list1 = [15, 22, 37, 64, 75,96,78]
+list2 =  [15, 22, 37, 64, 75,96,52]
+
+
+# list1 = [10, 20, 30, 40, 50,]
+# list2 = [10, 20, 30, 40, 50]
+
+
+if list1 == list2:
+    print("The lists are equal.")
+else:
+    print("The lists are not equal.")
