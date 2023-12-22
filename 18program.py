3fed199aa303f96df5d3073320421f2aece0ae17
Jatin Satani <jatinsatani111@gmail.com>
2023-12-22 15:40:03 +0530

    Done

---
+++ 18program.py
@@ -0,0 +1,8 @@
+listexample=[23,4,5,6,77,76,5,55,5,5,4]
+tupleexample=(3454,76,78,68,678,67967,96,7745,645,47)
+
+print(listexample)
+print(tupleexample)
+
+# list is mutable means after execution list you can change list 
+# tuple is imtable means after executin tuple you can't change 
