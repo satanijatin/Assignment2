78c49ed5cd53d303cb6d85d7299995094ebdc918
Jatin Satani <jatinsatani111@gmail.com>
2023-12-22 14:55:58 +0530

    Done

---
+++ 3program.py
@@ -0,0 +1,11 @@
+listpro=[3,4,5,6,7,89,34,56,398]
+
+listpro.append(44)
+listpro.extend("1111")
+print(listpro)
+
+
+#  In list append Method add value last of list and extend method one bye one value added into the list
+
+
+
