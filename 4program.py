65696afb48d4673d55b720d34353b4ef7cf765e1
Jatin Satani <jatinsatani111@gmail.com>
2023-12-22 14:58:57 +0530

    Done

---
+++ 4program.py
@@ -0,0 +1,8 @@
+
+numbers = [5,9,45,18,98,47,65,35,47,98,15]
+largestnumber = max(numbers)
+smallestnumber = min(numbers)
+totalsum = sum(numbers)
+print("Largest Number:", largestnumber)
+print("Smallest Number:", smallestnumber)
+print("Sum of All Numbers:", totalsum)
