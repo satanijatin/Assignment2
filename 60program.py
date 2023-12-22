3c1e70be194d46e34f87da0b511ee3abb25e78ea
Jatin Satani <jatinsatani111@gmail.com>
2023-12-22 15:36:35 +0530

    Done

---
+++ 60program.py
@@ -0,0 +1,7 @@
+base1 = float(input("Enter the length of the first base: "))
+base2 = float(input("Enter the length of the second base: "))
+height = float(input("Enter the height of the trapezoid: "))
+
+area = 0.5 * (base1 + base2) * height
+
+print("The area of the trapezoid is: ",area)
