07492a4a68e180b86ae0cb5dfc6163ef4563c61d
Jatin Satani <jatinsatani111@gmail.com>
2023-12-22 15:45:09 +0530

    Done

---
+++ 22program.py
@@ -0,0 +1,11 @@
+
+my_tuple = (10, 20, 30, 40, 50)
+
+
+element_to_check = int(input("Enter the element to check: "))
+
+
+if element_to_check in my_tuple:
+    print( "element exists in the tuple.")
+else:
+    print( "element does not exist in the tuple.")
