326f37fe12efaddc5119f368b5f46f7672846931
Jatin Satani <jatinsatani111@gmail.com>
2023-12-27 18:10:02 +0530

    Done

---
+++ 17program.py
@@ -0,0 +1,11 @@
+
+originallist = [10, 25, 38, 47, 59]
+
+
+var1, var2, var3, var4, var5 = originallist
+
+print("Variable 1:", var1)
+print("Variable 2:", var2)
+print("Variable 3:", var3)
+print("Variable 4:", var4)
+print("Variable 5:", var5)
