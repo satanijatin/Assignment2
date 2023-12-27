0865612f3d55ff3e6d84462b0453fafb93f742b4
Jatin Satani <jatinsatani111@gmail.com>
2023-12-27 18:07:25 +0530

    Done

---
+++ 15program.py
@@ -0,0 +1,7 @@
+originallist = [1, 2, 2, 3, 4, 4, 5, 6, 6, 7]
+uniqueset = set(originallist)
+
+uniquelist = list(uniqueset)
+
+print("Original List:", originallist)
+print("Unique Values:", uniquelist)
