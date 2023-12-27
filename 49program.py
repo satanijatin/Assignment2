bbb939612f319b4a568f2cc7ce3009e29ab98bb5
Jatin Satani <jatinsatani111@gmail.com>
2023-12-27 14:51:04 +0530

    Done

---
+++ 49program.py
@@ -0,0 +1,13 @@
+
+def test_range(n):
+    
+    if n in range(3, 9):
+       
+        print("number is in the range")
+    else:
+       
+        print("The number is outside the given range.")
+
+number=int(input("Enter Number: "))
+test_range(number)
+
