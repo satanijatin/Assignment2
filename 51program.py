30c32dc54970f1f8a4e17522cb0fe0074a694405
Jatin Satani <jatinsatani111@gmail.com>
2023-12-27 15:00:48 +0530

    Done

---
+++ 51program.py
@@ -0,0 +1,12 @@
+
+def isPalindrome(s):
+	return s == s[::-1]
+
+
+s = str(input("Enter String : "))
+ans = isPalindrome(s)
+
+if ans:
+	print("String is Palindrom")
+else:
+	print("String is Not Palindrom")
