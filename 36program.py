e96a73a9be530f117a76b95ed36ea8e3d5ebeb3d
Jatin Satani <jatinsatani111@gmail.com>
2023-12-28 10:01:30 +0530

    Done

---
+++ 36program.py
@@ -0,0 +1,11 @@
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+
+key_to_check = 'b'
+
+
+if key_to_check in my_dict:
+    print("The key  exists in the dictionary.")
+else:
+    print("The key  does not exist in the dictionary.")
