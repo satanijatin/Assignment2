0579ac63bb9b19a607d924085c12c24c8f469acc
Jatin Satani <jatinsatani111@gmail.com>
2023-12-28 10:07:39 +0530

    Done

---
+++ 42program.py
@@ -0,0 +1,12 @@
+
+my_dictionary = {'a': 1, 'b': 2, 'c': 1, 'd': 3, 'e': 2, 'f': 4}
+
+
+unique_values = set()
+
+
+for value in my_dictionary.values():
+    unique_values.add(value)
+
+
+print("Unique values in the dictionary:", list(unique_values))
