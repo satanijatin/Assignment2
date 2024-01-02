e77d4b1408e09fb77a2dbc7238debe7bdeb55340
Jatin Satani <jatinsatani111@gmail.com>
2024-01-02 11:33:35 +0530

    Done

---
+++ 12program.py
@@ -0,0 +1,11 @@
+
+char_list = ['a', 'b', 'c', 'd', 'e']
+
+
+result_string = ""
+for char in char_list:
+    result_string += char
+
+
+print("Original list of characters:", char_list)
+print("Resulting string:", result_string)
