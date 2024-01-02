416639c554d0bcd9ee214ff1433cea6f44c7680b
Jatin Satani <jatinsatani111@gmail.com>
2024-01-02 14:42:19 +0530

    Done

---
+++ 53program.py
@@ -0,0 +1,12 @@
+import random
+
+my_list = [1, 2, 3, 4, 5]
+random_item = random.choice(my_list)
+
+print(random_item)
+
+
+my_tuple = (6, 7, 8, 9, 10)
+random_item_tuple = random.choice(my_tuple)
+
+print(random_item_tuple)
