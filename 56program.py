f2a476bff082eaf5f4ef3b7849ae1f6c6eb72c02
Jatin Satani <jatinsatani111@gmail.com>
2024-01-02 14:46:32 +0530

    Done

---
+++ 56program.py
@@ -0,0 +1,9 @@
+import random
+
+random.seed(42)
+
+random_float = random.random()
+print("Random Float:", random_float)
+
+random_integer = random.randint(1, 10)
+print("Random Integer:", random_integer)
