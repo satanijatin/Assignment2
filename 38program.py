c02e2be528653623dc29860b976e058b8e50c5d3
Jatin Satani <jatinsatani111@gmail.com>
2024-01-02 14:34:59 +0530

    Done

---
+++ 38program.py
@@ -0,0 +1,10 @@
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+keys_to_check = ['a', 'c', 'f']
+
+
+for key in keys_to_check:
+    if key in my_dict:
+        print(f"Key '{key}' exists in the dictionary with value: {my_dict[key]}")
+    else:
+        print(f"Key '{key}' does not exist in the dictionary")
+
