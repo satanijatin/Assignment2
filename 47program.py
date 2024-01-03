bd01053431970230e54f942f58ddfa05940a989c
Jatin Satani <jatinsatani111@gmail.com>
2024-01-03 15:30:50 +0530

    Done

---
+++ 47program.py
@@ -0,0 +1,19 @@
+def create_dictionary_from_string(input_string):
+    result_dict = {}
+
+    for char in input_string:
+        if char in result_dict:
+            result_dict[char] += 1
+        else:
+            result_dict[char] = 1
+
+    return result_dict
+
+
+sample_string = 'w3resource'
+
+
+result = create_dictionary_from_string(sample_string)
+
+
+print(result)
