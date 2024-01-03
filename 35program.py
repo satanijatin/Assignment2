00ec5a3ae7477b4909a5e54fe5d2d83969667842
Jatin Satani <jatinsatani111@gmail.com>
2024-01-03 10:41:12 +0530

    Done

---
+++ 35program.py
@@ -0,0 +1,10 @@
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+for key in my_dict:
+    print(key)
+
+for value in my_dict.values():
+    print(value)
+
+for key, value in my_dict.items():
+    print(key, value)
