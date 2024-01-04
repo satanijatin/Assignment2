c464aceb02f52d94b436641154db8293c7790f21
Jatin Satani <jatinsatani111@gmail.com>
2024-01-04 12:29:52 +0530

    Done

--- 27program.py
+++ 27program.py
@@ -1,17 +1,14 @@
 
 my_tuple = (1, 2, 3, 2, 4, 5, 6, 3, 7, 8, 9, 9)
 
-
 repeated_items = set()
 unique_items = set()
-
 
 for item in my_tuple:
     if item in unique_items:
         repeated_items.add(item)
     else:
         unique_items.add(item)
-
 
 print("Original Tuple:", my_tuple)
 print("Repeated Items:", repeated_items)
