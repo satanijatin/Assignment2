edff0d808637746ea8fe030d55277f95f8153f37
Jatin Satani <jatinsatani111@gmail.com>
2024-01-04 12:32:01 +0530

    Update 46program.py

--- 46program.py
+++ 46program.py
@@ -14,8 +14,6 @@ sample_data = [{'item': 'item1', 'amount': 400},
                {'item': 'item2', 'amount': 300},
                {'item': 'item1', 'amount': 750}]
 
-
 result = combine_values(sample_data)
-
 
 print(result)
