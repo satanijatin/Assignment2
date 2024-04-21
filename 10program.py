7de45ae744697a7bd1af1c9f0b5627391d81c7b9
Jatin Satani <jatinsatani111@gmail.com>
2024-04-21 13:51:21 +0530

    Done

--- 10program.py
+++ 10program.py
@@ -1,8 +1,16 @@
 
-squares_list = [i**2 for i in range(1, 31)]
+# squares_list = [i**2 for i in range(1, 31)]
+# print("First 5 elements:", squares_list[:5])
+# print("Last 5 elements:", squares_list[-5:])
 
+def rearrange_list(lst):
+    for i in range(1, len(lst), 2):
+        print( lst[i], lst[i-1])
+        print( lst[i-1], lst[i])
+        lst[i], lst[i-1] = lst[i-1], lst[i]
+    return lst
 
-print("First 5 elements:", squares_list[:5])
-
-
-print("Last 5 elements:", squares_list[-5:])
+# Test the function
+input_list = [5, 10, 5, 12, 13, 14]
+output_list = rearrange_list(input_list)
+print(output_list)
--- 1program.py
+++ 1program.py
@@ -1,2 +1,10 @@
-numberlist=[89,52,68,98,97,69,56,63,35,25,24,15,16,58]
-print(numberlist[::-1])
+# numberlist=[89,52,68,98,97,69,56,63,35,25,24,15,16,58]
+# print(numberlist[::-1])
+
+for i in range(0,5):
+    for j in range(0,8):
+        if j>=i and j<=8-i:
+            print("*", end=" ")
+        else:
+             print(" " ,end=" ")
+    print()
